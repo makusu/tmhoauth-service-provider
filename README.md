189e33d0663d3a88a56278048fb4dd3485bfa4b3
Max Meiden Dasuki <max@meidendasuki.com>
2014-01-01 04:27:02 +0800

    Commit the code

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-tmhoauth-service-provider
-=========================
+# tmhOAuth Silex Extension
 
-tmhOAuth service provider for Silex.
+tmhOAuth service provider for [Silex][1].
+
+## Installation
+
+```bash
+$ wget http://getcomposer.org/composer.phar
+$ php composer.phar install
+```
+
+## Registering
+
+```php
+$app->register(new TmhOAuth\Silex\Provider\TmhOAuthServiceProvider(), array(
+    'tmhoauth.keys' => array(
+    	'consumer_key'    => 'ENTER_HERE_YOUR_TWITTER_CONSUMER_KEY',
+    	'consumer_secret   => 'ENTER_HERE_YOUR_TWITTER_CONSUMER_SECRET',
+    ),
+));
+```
+
+## Credits
+
+* [tmhOAuth][2]
+
+## License
+
+The tmhOAuth Silex Extension is licensed under the MIT license.
+
+[1]: http://silex-project.org
+[2]: https://github.com/themattharris/tmhOAuth
---
+++ src/TmhOAuth/Silex/Provider/TmhOAuthServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace TmhOAuth\Silex\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Themattharris\TmhOAuth;
+use Themattharris\TmhUtilities;
+
+class TmhOAuthServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        if (!isset($app['tmhoauth.config']['consumer_key'], $app['tmhoauth.config']['consumer_secret'])) {
+            throw new \RuntimeException('Config must include consumer_key and consumer_secret');
+        }
+
+        $app['tmhoauth'] = $app->share(function () use ($app) {
+            return new TmhOAuth($app['tmhoauth.config']);
+        });
+
+        $app['tmhoauth.utils'] = $app->share(function () {
+            return new TmhUtilities();
+        });
+    }
+
+    public function boot(Application $app)
+    {
+    }
+}
